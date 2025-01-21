d1e014a69dbb74856ee9ed45b2b6e43aba2bc891
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2025-01-21 12:00:59 +0100

    x

--- backend/functions/hello/hello.controller.js
+++
@@ -1,8 +0,0 @@
-// Controller logic for hello route
-const sayHello = (req, res) => {
-  res.json({ message: "Hello from the Hello module!" });
-};
-
-module.exports = {
-  sayHello,
-};
--- backend/functions/hello/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { sayHello } = require("./hello.controller");
-
-const router = express.Router();
-
-// Define routes for "hello"
-router.get("/", sayHello);
-
-module.exports = router;
