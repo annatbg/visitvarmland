292c995bf238bd3fec690d069174de60830d94a3
annathunberg <anna.thunberg1@gmail.com>
2025-01-27 18:02:52 +0100

    'update'

--- frontend/src/components/nav/TopNav/TopNav.jsx
+++ frontend/src/components/nav/TopNav/TopNav.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./TopNav.css";
+import useUser from "../../../store/useUser";
 
 const TopNav = () => {
+  const user = useUser((state) => state.user);
+  const logout = useUser((state) => state.logout);
+
+  const handleLogout = () => {
+    logout();
+    localStorage.removeItem("token");
+    navigate("/");
+    console.log("Logged out");
+  };
+
   return (
     <nav className="topNav">
       <ul className="nav-list">
+        <li className="nav-item">{user}</li>
         <li className="nav-item">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/services" className="nav-link">
-            Services
-          </Link>
-        </li>
-        <li className="nav-item">
-          <Link to="/contact" className="nav-link">
-            Contact
-          </Link>
+          <button
+            onClick={handleLogout}
+            className="contentPage__user-logoutBTN"
+          >
+            Logout
+          </button>
         </li>
       </ul>
     </nav>
