bff513c047617e2389d4d65c2118f9618dd66816
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2025-02-01 17:23:34 +0100

    'update'

--- frontend/src/components/forms/createDemand/CreateDemand.jsx
+++ frontend/src/components/forms/createDemand/CreateDemand.jsx
@@ -12,6 +12,8 @@ const CreateDemand = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const [successMessage, setSuccessMessage] = useState(null);
+  const [isFormOpen, setIsFormOpen] = useState(false);
+
   const user = useUser((state) => state.user);
 
   const handleChange = (e) => {
@@ -54,55 +56,69 @@ const CreateDemand = () => {
 
   return (
     <>
-      <h3>Create Demand</h3>
-      <form onSubmit={handleSubmit} className="demandview-form">
-        <label htmlFor="title" className="demandview-label">
-          Title:
-        </label>
-        <input
-          type="text"
-          id="title"
-          value={formData.title}
-          onChange={handleChange}
-          placeholder="Enter a title"
-          className="demandview-input"
-          disabled={loading}
-        />
+      <h3
+        className="demandForm-heading"
+        onClick={() => setIsFormOpen(!isFormOpen)}
+      >
+        Skapa nytt behov
+        <span className="demandForm-arrow">{isFormOpen ? "▲" : "▼"}</span>
+      </h3>
 
-        <label htmlFor="demand" className="demandview-label">
-          Demand:
-        </label>
-        <input
-          type="text"
-          id="demand"
-          value={formData.demand}
-          onChange={handleChange}
-          placeholder="Write your demand here..."
-          className="demandview-input"
-          disabled={loading}
-        />
+      {isFormOpen && (
+        <form onSubmit={handleSubmit} className="demandForm-form">
+          <label htmlFor="title" className="demandForm-label">
+            Rubrik:
+          </label>
+          <input
+            type="text"
+            id="title"
+            value={formData.title}
+            onChange={handleChange}
+            placeholder="Skriv en rubrik"
+            className="demandForm-input"
+            disabled={loading}
+          />
 
-        <label htmlFor="category" className="demandview-label">
-          Category:
-        </label>
-        <input
-          type="text"
-          id="category"
-          value={formData.category}
-          onChange={handleChange}
-          placeholder="Enter category"
-          className="demandview-input"
-          disabled={loading}
-        />
+          <label htmlFor="demand" className="demandForm-label">
+            Behov:
+          </label>
+          <input
+            type="text"
+            id="demand"
+            value={formData.demand}
+            onChange={handleChange}
+            placeholder="Beskriv ditt behov..."
+            className="demandForm-input"
+            disabled={loading}
+          />
 
-        {error && <p className="demandview-error">{error}</p>}
-        {successMessage && (
-          <p className="demandview-success">{successMessage}</p>
-        )}
-        <button type="submit" className="demandview-button" disabled={loading}>
-          {loading ? "Submitting..." : "Create Demand"}
-        </button>
-      </form>
+          <label htmlFor="category" className="demandForm-label">
+            Kategori:
+          </label>
+          <input
+            type="text"
+            id="category"
+            value={formData.category}
+            onChange={handleChange}
+            placeholder="Vilken kategori tillhör ditt behov"
+            className="demandForm-input"
+            disabled={loading}
+          />
+
+          {error && <p className="demandForm-error">{error}</p>}
+          {successMessage && (
+            <p className="demandForm-success">{successMessage}</p>
+          )}
+
+          <button
+            type="submit"
+            className="demandForm-button"
+            disabled={loading}
+          >
+            {loading ? "Submitting..." : "Skapa behov"}
+          </button>
+        </form>
+      )}
     </>
   );
 };
