1c579a999f33a5a6b03c971b48c4f777e1fbb6a8
pettersson932 <144891931+pettersson932@users.noreply.github.com>
2025-02-07 08:18:59 +0100

    'update'

--- backend/services/utils/jwt.js
+++ backend/services/utils/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret_key";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 const generateToken = (username) => {
   return jwt.sign({ username }, JWT_SECRET, {
